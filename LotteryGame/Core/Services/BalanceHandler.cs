6d261a512aa7ee63880a098cd38ea5e4bad05f69
OCattaneo <91796627+OCattaneo@users.noreply.github.com>
2024-11-13 20:54:08 +0000

    Add Ticket Handling & generation (#2)

---
+++ LotteryGame/Core/Services/BalanceHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Services
+{
+    public class BalanceHandler
+    {
+        public BalanceHandler()
+        {
+        }
+
+        public decimal[] SubtractTickets(int[] ticketCounts, decimal[] balanceArray)
+        {
+            for (int i = 0; i < balanceArray.Length; i++)
+            {
+                balanceArray[i] = balanceArray[i] - ticketCounts[i];
+            }
+
+            return balanceArray;
+        }
+    }
+}
